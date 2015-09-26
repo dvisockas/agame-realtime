0491ede614dd3a81304e042f1c6df9f55c5c219f
Rokas Milasevicius <rokas@milasevicius.com>
2015-09-26 21:27:01 +0300

    Refactored code

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 1234;
 var Server = require('./Server.js');
 var User = require('./User.js');
 
@@ -17,30 +17,24 @@ app.use(express.static(__dirname + '/public'));
 var gameServer = new Server();
 
 io.on('connection', function (socket) {
-    //User creates his account
+
     socket.on('login', function (user) {
         socket.currentUser = new User();
-        socket.currentUser.username = user.username;
-        socket.currentUser.location = user.location;
+        socket.currentUser = user;
 
         gameServer.addUser(socket.currentUser);
 
         socket.broadcast.emit('user joined', {
             user: socket.currentUser
         });
-
-        console.log("User " + socket.currentUser.username + " connected. Count: " + gameServer.users.length);
     });
 
-    //User disconnects from game
     socket.on('disconnect', function () {
         gameServer.removeUser(socket.currentUser);
 
         socket.broadcast.emit('user left', {
             user: socket.currentUser
         });
-
-        console.log("User " + socket.currentUser.username + " disconnected. Count: " + gameServer.users.length);
     });
 
     socket.on('location changed', function (user) {
